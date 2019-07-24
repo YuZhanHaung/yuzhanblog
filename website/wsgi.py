6f183dc9f92ac951d8d4705d707ad17ad4318178
unknown <bigjoe1992111@gmail.com>
2019-07-24 15:01:39 +0800

    my djangoboys app

--- website/wsgi.py
+++ website/wsgi.py
@@ -10,12 +10,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
-
-
-application = DjangoWhiteNoise(application) #heroku add
+application = Cling(get_wsgi_application())
